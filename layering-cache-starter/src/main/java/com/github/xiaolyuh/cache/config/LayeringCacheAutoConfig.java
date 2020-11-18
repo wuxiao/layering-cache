a4c0a31847976f300ddec05b7398437fd97fc0b8
伟 王 <wei.wang@shuyun.com>
2020-11-18 19:05:03 +0800

    fix: 修复缓存不生效问题

--- layering-cache-starter/src/main/java/com/github/xiaolyuh/cache/config/LayeringCacheAutoConfig.java
+++ layering-cache-starter/src/main/java/com/github/xiaolyuh/cache/config/LayeringCacheAutoConfig.java
@@ -4,8 +4,8 @@ import com.github.xiaolyuh.aspect.LayeringAspect;
 import com.github.xiaolyuh.cache.properties.LayeringCacheProperties;
 import com.github.xiaolyuh.manager.CacheManager;
 import com.github.xiaolyuh.manager.LayeringCacheManager;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 /**
  * 多级缓存自动配置类
@@ -21,8 +21,7 @@ import org.springframework.data.redis.core.RedisTemplate;
  * @author xiaolyuh
  */
 @Configuration
-@ConditionalOnBean(RedisTemplate.class)
-@AutoConfigureAfter({RedisAutoConfiguration.class})
+@AutoConfigureAfter({RedisAutoConfiguration.class, RedissonAutoConfiguration.class})
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties({LayeringCacheProperties.class})
 @Import({LayeringCacheServletConfiguration.class})
@@ -30,8 +29,8 @@ public class LayeringCacheAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean(CacheManager.class)
-    public CacheManager layeringCacheManager(RedisTemplate<String, Object> redisTemplate, LayeringCacheProperties properties) {
-        LayeringCacheManager layeringCacheManager = new LayeringCacheManager(redisTemplate);
+    public CacheManager layeringCacheManager(StringObjectRedisTemplate stringObjectRedisTemplate, LayeringCacheProperties properties) {
+        LayeringCacheManager layeringCacheManager = new LayeringCacheManager(stringObjectRedisTemplate);
         // 默认开启统计功能
         layeringCacheManager.setStats(properties.isStats());
         return layeringCacheManager;
@@ -42,4 +41,8 @@ public class LayeringCacheAutoConfig {
         return new LayeringAspect();
     }
 
+    @Bean
+    public StringObjectRedisTemplate stringObjectRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new StringObjectRedisTemplate(redisConnectionFactory);
+    }
 }
---
+++ layering-cache-starter/src/main/java/com/github/xiaolyuh/cache/config/StringObjectRedisTemplate.java
@@ -0,0 +1,50 @@
+package com.github.xiaolyuh.cache.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * @author wayne 2020/11/18
+ */
+public class StringObjectRedisTemplate extends RedisTemplate<String, Object> {
+    public StringObjectRedisTemplate() {
+        Jackson2JsonRedisSerializer<Object> serializer = serializer();
+        setKeySerializer(RedisSerializer.string());
+        setValueSerializer(serializer);
+        setHashKeySerializer(RedisSerializer.string());
+        setHashValueSerializer(serializer);
+    }
+
+    /**
+     * Constructs a new <code>StringRedisTemplate</code> instance ready to be used.
+     *
+     * @param connectionFactory connection factory for creating new connections
+     */
+    public StringObjectRedisTemplate(RedisConnectionFactory connectionFactory) {
+        this();
+        setConnectionFactory(connectionFactory);
+        afterPropertiesSet();
+    }
+
+    @Override
+    protected RedisConnection preProcessConnection(RedisConnection connection, boolean existingConnection) {
+        return connection;
+    }
+
+    private Jackson2JsonRedisSerializer<Object> serializer() {
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        mapper.registerModule(new JavaTimeModule());
+        serializer.setObjectMapper(mapper);
+        return serializer;
+    }
+}
